063c9616d69a9bf230c6a9ef5033d58e5b71af25
briandenicola <brian@briandenicola.com>
2024-02-08 12:09:55 -0600

    Bug fixes

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -3,6 +3,11 @@ variable "region" {
   default     =  "southcentralus"
 }
 
+variable "namespace" {
+  description = "The namespace to deploy Crossplane to"
+  default = "upbound-system"
+}
+
 variable "vm_sku" {
   description = "The VM type for the system node pool"
   default     = "Standard_D4ads_v5"
